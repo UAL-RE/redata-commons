35c2946e6f294a956ac55608bab40d6ed827a19c
Chun Ly <astro.chun@gmail.com>
2021-03-09 14:58:24 -0700

    Add unit tests for git_info module

---
+++ tests/test_git_info.py
@@ -0,0 +1,14 @@
+from redata_commons.git_info import GitInfo
+
+
+def test_GitInfo():
+    gi = GitInfo()
+
+    assert isinstance(gi.branch, str)
+
+    assert isinstance(gi.commit, str)
+    assert isinstance(gi.short_commit, str)
+    if not gi.short_commit:  # Empty string
+        assert len(gi.short_commit) == 0
+    else:
+        assert len(gi.short_commit) == 7
