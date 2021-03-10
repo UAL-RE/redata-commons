cf4ce72e6639c932eb17fc168467d9007fb5c0b5
Chun Ly <astro.chun@gmail.com>
2021-03-10 09:32:01 -0700

    git_info: Remove os library usage

--- redata_commons/git_info.py
+++ redata_commons/git_info.py
@@ -1,23 +1,21 @@
 from pathlib import Path
-from os.path import dirname, basename, exists, join
 
 # Get git root
-git_root = dirname(__file__.replace(f'/{basename(__file__)}', ''))
+git_root = Path(__file__).parent.parent
 
 
 class GitInfo:
     def __init__(self, input_path: str = git_root):
         self.input_path = input_path
+        self.head_path = Path(self.input_path) / ".git" / "HEAD"
         self.branch = self.get_active_branch_name()
         commit_tuple = self.get_latest_commit()
         self.commit = commit_tuple[0]
         self.short_commit = commit_tuple[1]
 
     def get_active_branch_name(self):
-
-        if exists(join(self.input_path, ".git", "HEAD")):
-            head_dir = Path(self.input_path) / ".git" / "HEAD"
-            with head_dir.open("r") as f:
+        if self.head_path.exists():
+            with self.head_path.open("r") as f:
                 content = f.read().splitlines()
 
             for line in content:
@@ -29,16 +27,14 @@ class GitInfo:
             return '.git structure does not exist'
 
     def get_latest_commit(self):
-
-        if exists(join(self.input_path, ".git", "HEAD")):
-            head_dir = Path(self.input_path) / ".git" / "HEAD"
-            with head_dir.open("r") as f:
+        if self.head_path.exists():
+            with self.head_path.open("r") as f:
                 content = f.read().splitlines()
 
             for line in content:
                 if line[0:4] == "ref:":
-                    head_path = Path(join(self.input_path, f".git/{line.partition(' ')[2]}"))
-                    with head_path.open('r') as g:
+                    ref_path = Path(self.input_path) / ".git" / f"{line.partition(' ')[2]}"
+                    with ref_path.open('r') as g:
                         commit = g.read().splitlines()
                 else:
                     commit = content
