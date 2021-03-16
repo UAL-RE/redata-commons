0ddd58541dd4ce9ce011edc368c714951d288fad
Chun Ly <astro.chun@gmail.com>
2021-03-16 15:02:44 -0700

    Add args to setup.py
     - Include project_urls, python_requires, and classifiers

--- setup.py
+++ setup.py
@@ -8,10 +8,22 @@ setup(
     version='0.0.1',
     packages=find_namespace_packages(),
     url='https://github.com/UAL-ODIS/redata_commons/',
+    project_urls={
+        'Source': 'https://github.com/UAL-ODIS/redata_commons/',
+        'Tracker': 'https://github.com/UAL-ODIS/redata_commons/issues',
+    },
     license='MIT License',
     author='Chun Ly',
     author_email='astro.chun@gmail.com',
     description='Commons code used by ReDATA software',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    python_requires='>=3.7',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
 )
