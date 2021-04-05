11864e60b994e269d523363962a9ffde30fd21db
Chun Ly <astro.chun@gmail.com>
2021-04-05 15:54:36 -0700

    Bump version: v0.2.0 -> v0.3.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='redata',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_namespace_packages(),
     url='https://github.com/UAL-ODIS/redata-commons/',
     project_urls={
