2295571f626e8ade089e45787aea1e9ed34f81bb
HafeezOJ <32125238+HafeezOJ@users.noreply.github.com>
2025-06-11 16:56:37 -0700

    Update pandas, tabulate, requests to latest version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r") as fr:
 
 setup(
     name='redata',
-    version='0.5.0',
+    version='0.4.1',
     packages=find_namespace_packages(),
     url='https://github.com/UAL-RE/redata-commons/',
     project_urls={
@@ -16,17 +16,16 @@ setup(
         'Tracker': 'https://github.com/UAL-RE/redata-commons/issues',
     },
     license='MIT License',
-    author='Yan Han',
-    author_email='yhan818@gmail.com',
+    author='UAL-RE',
+    author_email='redata@arizona.edu',
     description='Commons code used by ReDATA software',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    python_requires='>=3.8',
+    python_requires='>=3.9',
     install_requires=requirements,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
     ]
 )
